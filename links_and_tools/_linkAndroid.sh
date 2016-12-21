d9d0b5fb5cdb659bfe0a9f37429e80c2cbc29519
celly <celly@livesport.eu>
2016-12-21 13:59:06 +0100

    Add android link

---
+++ links_and_tools/_linkAndroid.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ln -s /Volumes/secure_disk/Android/security.properties /var/root/.jenkins/workspace/Android/apps/android/flashscore/
+ln -s /Volumes/secure_disk/Android/release.keystore /var/root/.jenkins/workspace/Android/apps/android/flashscore/fs_build/
