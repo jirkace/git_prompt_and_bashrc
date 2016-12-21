1bd7431ede96b5736e3097a6e24f6490c6e04b8a
celly <celly@livesport.eu>
2016-12-21 14:25:51 +0100

    Linkink edit

--- links_and_tools/_linkAndroid.sh
+++ links_and_tools/_linkAndroid.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+rm -f /var/root/.jenkins/workspace/Android/apps/android/flashscore/security.properties
+rm -f /var/root/.jenkins/workspace/Android/apps/android/flashscore/fs_build/release.keystore
+
 ln -s /Volumes/secure_disk/Android/security.properties /var/root/.jenkins/workspace/Android/apps/android/flashscore/
 ln -s /Volumes/secure_disk/Android/release.keystore /var/root/.jenkins/workspace/Android/apps/android/flashscore/fs_build/
