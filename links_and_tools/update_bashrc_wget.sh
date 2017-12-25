ddf5f3bb3e9f2b6fbc6311f3b84264e1240558dd
celly <celly@livesport.eu>
2017-12-25 19:44:15 +0100

    update bashrc celly wget

--- links_and_tools/update_bashrc_wget.sh
+++ links_and_tools/update_bashrc_wget.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-cd "$HOME"
+cd /tmp
+rm -rf master
 wget https://codeload.github.com/bbcelly/git_prompt_and_bashrc/zip/master
 mv master master.zip
-unzip master.zip 
-mv git_prompt_and_bashrc-master .bashrc_celly
+unzip master.zip
+rm -rf master.zip "$HOME/.bashrc_celly"
+mv git_prompt_and_bashrc-master "$HOME/.bashrc_celly"
