458b2357715a862d889e65ee9b2920953c2e2813
bbcelly <bbcelly@users.noreply.github.com>
2018-04-05 11:19:15 +0200

    Moved android to own repo

--- links_and_tools/_linkAndroid.sh
+++ links_and_tools/_linkAndroid.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-rm -f /Users/jarvis/.jenkins/workspace/Android/apps/android/flashscore/security.properties
-rm -f /Users/jarvis/.jenkins/workspace/Android/apps/android/flashscore/fs_build/release.keystore
+rm -f /Users/jarvis/.jenkins/workspace/Android/flashscore/security.properties
+rm -f /Users/jarvis/.jenkins/workspace/Android/flashscore/fs_build/release.keystore
 
-ln -s /Volumes/secure_disk/Android/security.properties /Users/jarvis/.jenkins/workspace/Android/apps/android/flashscore/
-ln -s /Volumes/secure_disk/Android/release.keystore /Users/jarvis/.jenkins/workspace/Android/apps/android/flashscore/fs_build/
+ln -s /Volumes/secure_disk/Android/security.properties /Users/jarvis/.jenkins/workspace/Android/flashscore/
+ln -s /Volumes/secure_disk/Android/release.keystore /Users/jarvis/.jenkins/workspace/Android/flashscore/fs_build/
